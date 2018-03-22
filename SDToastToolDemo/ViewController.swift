9c5bdc33300240943cbad68e1aba2b5dc487dbf3
庞翔 <pangxiangios@163.com>
2018-03-22 22:42:41 +0800

    VTPopupBaseView 完成

--- SDToastToolDemo/ViewController.swift
+++ SDToastToolDemo/ViewController.swift
@@ -9,17 +9,20 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var contentView = UIView.init(frame: CGRect.init(x: 100, y: 100, width: 300, height: 300))
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.view.backgroundColor = UIColor.white
+        contentView.backgroundColor = UIColor.yellow
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesBegan(touches, with: event)
+    }
 }
 
